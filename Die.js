147963e835764235bd9a9d72e8b22eb4e452fa8c
Giovanni L <callmegiorgio@hotmail.com>
2022-11-09 20:53:04 -0300

    Put real dots on the dice

--- Die.js
+++ Die.js
@@ -4,13 +4,21 @@ export default function Die(props) {
     const styles = {
         backgroundColor: props.isHeld ? "#59E391" : "white"
     }
+
+    const dotElements = []
+
+    for (let i = 0; i < props.value; i++) {
+        // https://dev.to/ekeijl/creating-dice-using-css-grid-j4
+        dotElements.push(<span className="die-dot"></span>)
+    }
+
     return (
         <div 
             className="die-face" 
             style={styles}
             onClick={props.holdDice}
         >
-            <h2 className="die-num">{props.value}</h2>
+            {dotElements}
         </div>
     )
 }
